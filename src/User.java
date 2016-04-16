cad2d4dcee6c6427dc7e01966d2a3da4e2604326
arnoldmanzano <arnold.manzano@gmail.com>
2016-04-16 10:11:15 +0100

    Initial commit, User class created

---
+++ src/User.java
@@ -0,0 +1,9 @@
+/**
+ * Created by arnold on 16/4/16.
+ */
+public class User {
+    public int[] view_timeline() {
+        int[] posts = {};
+        return posts;
+    }
+}
---
+++ test/UserTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+/**
+ * Created by arnold on 16/4/16.
+ */
+public class UserTest {
+
+    @Test
+    public void NewUser() throws Exception {
+        User user = new User();
+        int[] emptyArray = {};
+        assertArrayEquals(emptyArray, user.view_timeline());
+    }
+
+}
