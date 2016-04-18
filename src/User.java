9b9a615db1c8693cd5ca0e2a8a5983b6224912d8
arnoldmanzano <arnold.manzano@gmail.com>
2016-04-18 09:36:34 +0100

    Following feature implemented, User #addFollowing method implemented and tested

--- src/User.java
+++ src/User.java
@@ -4,14 +4,20 @@ public class User {
 
     private String name;
     private ArrayList<Post> posts;
+    private ArrayList<User> following;
 
     public User(String name) {
         this.posts = new ArrayList<>();
+        this.following = new ArrayList<>();
         this.name = name;
     }
 
     public void addPost(String message) {
         posts.add(new Post(message));
+    }
+
+    public void addFollowing(User user) {
+        following.add(user);
     }
 
     public ArrayList<Post> getPosts() {
@@ -22,4 +28,7 @@ public class User {
         return name;
     }
 
+    public ArrayList<User> getFollowing() {
+        return following;
+    }
 }
--- test/UserTest.java
+++ test/UserTest.java
@@ -1,14 +1,26 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class UserTest {
 
     @Test
-    public void addPost() throws Exception {
+    public void addPostTest() throws Exception {
         User user = new User("my_username");
         assertTrue(user.getPosts().isEmpty());
         user.addPost("post message");
         assertFalse(user.getPosts().isEmpty());
     }
 
+    @Test
+    public void addFollowingTest() throws Exception {
+        User userToFollow = new User("userToFollow");
+        User user = new User("myUserName");
+        user.addFollowing(userToFollow);
+
+        ArrayList<User> expectedCollection = new ArrayList<>();
+        expectedCollection.add(userToFollow);
+        assertEquals(user.getFollowing(), expectedCollection);
+    }
 }
